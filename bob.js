c157afb1ca43f4e162cf4aa84960f7410b35c0c1
aarshiya6302 <70167433+aarshiya6302@users.noreply.github.com>
2020-11-22 13:53:44 -0600

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,30 @@
+class Bob {
+    constructor(x, y, r) {
+      var options = {
+          isStatic:false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+
+      this.body = Bodies.circle(x, y, r/2, options);
+      this.width = 100;
+      this.height = 100;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(CENTER);
+      fill("violet");
+      ellipse(0,0);
+      pop();
+    }
+  };
+  
+  
+  
